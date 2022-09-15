07570c943181e262637a911424f2733b3a4c3341
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-15 08:33:27 +0200

    resetting the Welcome

--- store-language-app/src/components/Welcome.js
+++ store-language-app/src/components/Welcome.js
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import GreetingContext from "./GreetingContext";
-
 function Welcome() {
   const greetingText = useContext(GreetingContext);
+
   return (
     <div>
       <br />
@@ -11,3 +11,20 @@ function Welcome() {
   );
 }
 export default Welcome;
+
+/* Without useContext - get props from App.js 
+
+function Welcome({ options, value }) {
+
+  let greeting = "";
+  options.forEach(opt => {
+    if (opt.value === value) greeting = opt.banner;
+  });
+  return (
+    <div>
+      <br />
+      <h1 style={{ fontSize: "5rem" }}>{greeting}</h1>
+    </div>
+  );
+}
+*/
