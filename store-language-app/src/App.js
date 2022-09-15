f69e879bdbb09615ad092a0ec7d347344b379a8f
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-15 08:34:23 +0200

    resetting the App.js

--- store-language-app/src/App.js
+++ store-language-app/src/App.js
@@ -1,27 +1,42 @@
-import React,{useState} from "react";
+import React from "react";
 import "./App.css";
 import Dropdown from "./components/Dropdown";
-import options from "./components/Options";
 
 function App() {
-  const [value, setValue] = useState("english")
-
-  function handleChange(e){
-      setValue(e.target.value)
-  }
- 
   return (
-      <div className="App">
-          <h1 className="header">Language Chooser</h1>
-          <label>Please select your language</label>
-              <Dropdown
-              className="dropdown"
-              options={options}
-              value = {value}
-              onChange = {handleChange}
-              />
-      </div>
+    <div className="App">
+      <h1 className="header">Language Chooser</h1>
+      <label>Please select your language</label>
+      <Dropdown />
+    </div>
   );
 }
 
 export default App;
+
+/* without useContext - we have to 
+1. import data from options
+2. import Welcome component
+2. set the props for Welcome
+
+import options from "./components/Options";
+import Welcome from "./components/Welcome";
+
+function App() {
+  const [value, setValue] = useState("english");
+
+  return (
+    <div className="App">
+      <h1 className="header">Language Chooser</h1>
+      <label>Please select your language</label>
+      <Dropdown
+        className="dropdown"
+        options={options}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <Welcome options={options} value={value} />
+    </div>
+  );
+}
+*/
