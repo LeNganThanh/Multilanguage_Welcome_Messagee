32ddd1146c54683a15c8d0048b0793b21da322bc
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-15 08:32:43 +0200

    resetting the Dropdown

--- store-language-app/src/components/Dropdown.js
+++ store-language-app/src/components/Dropdown.js
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import Welcome from "./Welcome";
 import GreetingContext from "./GreetingContext";
+import options from "./Options";
 
-const Dropdown = ({ value, options, onChange }) => {
+const Dropdown = () => {
+  const [value, setValue] = useState("english");
+
+  //greeting for Welcome component
   const greeting = options.map(opt => (opt.value === value ? opt.banner : ""));
+
   return (
     <GreetingContext.Provider value={greeting}>
       <div>
-        <select value={value} onChange={onChange}>
-          {options.map(option => (
-            <option value={option.value}>{option.value} </option>
+        <select onChange={e => setValue(e.target.value)}>
+          {options.map((option, id) => (
+            <option key={id} value={option.value}>
+              {option.value}{" "}
+            </option>
           ))}
         </select>
-        {/* without using useContext can call greeting directly here
-     <p>{greeting}</p> */}
         <Welcome />
       </div>
     </GreetingContext.Provider>
   );
 };
 export default Dropdown;
+
+/* Without using useContext - setting only for Dropdown
+
+const Dropdown = ({ value, options, onChange }) => {
+  return (
+    <div>
+      <select value={value} onChange={onChange}>
+        {options.map((option, id) => (
+          <option key={id} value={option.value}>
+            {option.value}{" "}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+export default Dropdown;
+*/
+
+/* className="dropdown"
+options={options}
+value={value}
+
+ */
